59dfc0adf68b95d2a98a8f97f317099c6b33dd19
Yonatan Lapidus <lapidusy@gmail.com>
2020-06-10 16:08:56 +0300

    Added EjectedPilotRescueController and info mapping inside

---
+++ src/main/java/iaf/ofek/hadracha/base_course/web_server/EjectedPilotRescue/EjectedPilotRescueController.java
@@ -0,0 +1,9 @@
+package iaf.ofek.hadracha.base_course.web_server.EjectedPilotRescue;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ejectedPilotRescue")
+public class EjectedPilotController {
+}
